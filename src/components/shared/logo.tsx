1abca289e620c0c1d3520d2ec0aebd69d7c96497
Justin Shaw <justinshawcareer@gmail.com>
2026-04-02 23:57:00 -0400

    refactor: Add new logo

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
@@ -13,33 +14,21 @@ function Logo({ className, showWordmark = true }: LogoProps) {
       className={cn("flex items-center gap-2", className)}
       aria-label="Bizzed home"
     >
-      <svg
-        width="28"
-        height="28"
-        viewBox="0 0 28 28"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-        className="shrink-0"
-      >
-        <rect
-          width="28"
-          height="28"
-          rx="7"
-          className="fill-foreground"
-        />
-        <path
-          d="M8 8h4.5a3.5 3.5 0 0 1 .5 6.96V15a3.5 3.5 0 0 1-1 6.88 3.5 3.5 0 0 1-.5.12H8V8Zm2 2v4h2.5a1.5 1.5 0 1 0 0-3H10Zm0 6v4h3a1.5 1.5 0 1 0 0-3h-3Z"
-          className="fill-background"
-          fillRule="evenodd"
-          clipRule="evenodd"
-        />
-      </svg>
       {showWordmark && (
-        <span className="text-lg font-semibold tracking-tight text-foreground">
+        <span className="translate-y-1 text-lg font-semibold leading-none tracking-tight text-foreground">
           Bizzed
         </span>
       )}
+      <Image
+        src="/logo-mark.png"
+        alt=""
+        width={40}
+        height={40}
+        className="size-10 shrink-0 dark:invert"
+        priority
+        aria-hidden
+      />
+
     </Link>
   );
 }
